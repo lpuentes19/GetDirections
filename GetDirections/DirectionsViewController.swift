1d03fb85774d81f356542b35fb09268253670be7
lpuentes19 <luey.puentes@gmail.com>
2017-07-21 15:41:31 -0600

    Created the map view with my location being shown. Currently have it set to show directions to  Sports Authority Field Denver, CO

---
+++ GetDirections/DirectionsViewController.swift
@@ -0,0 +1,72 @@
+//
+//  DirectionsViewController.swift
+//  GetDirections
+//
+//  Created by Luis Puentes on 7/21/17.
+//  Copyright © 2017 LuisPuentes. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class DirectionsViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+
+    let locationManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        mapView.delegate = self
+        mapView.showsScale = true
+        mapView.showsPointsOfInterest = true
+        mapView.showsUserLocation = true
+        
+        locationManager.requestAlwaysAuthorization()
+        locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
+        }
+        
+        let sourceCoordinates = locationManager.location?.coordinate
+        let destCoordinates = CLLocationCoordinate2DMake(36.1070, -122.1130)
+        
+        let sourcePlacemark = MKPlacemark(coordinate: sourceCoordinates!)
+        let destPlacemark = MKPlacemark(coordinate: destCoordinates)
+        
+        let sourceItem = MKMapItem(placemark: sourcePlacemark)
+        let destItem = MKMapItem(placemark: destPlacemark)
+        
+        let directionRequest = MKDirectionsRequest()
+        directionRequest.source = sourceItem
+        directionRequest.destination = destItem
+        directionRequest.transportType = .automobile
+        
+        let directions = MKDirections(request: directionRequest)
+        directions.calculate { (response, error) in
+            guard let response = response else {
+                if let error = error {
+                    print("Something went wrong")
+                }
+                return
+            }
+            
+            let route = response.routes[0]
+            self.mapView.add(route.polyline, level: .aboveRoads)
+            
+            let rekt = route.polyline.boundingMapRect
+            self.mapView.setRegion(MKCoordinateRegionForMapRect(rekt), animated: true)
+        }
+    }
+    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        let renderer = MKPolylineRenderer(overlay: overlay)
+        renderer.strokeColor = .blue
+        renderer.lineWidth = 5.0
+        
+        return renderer
+    }
+    @IBOutlet weak var mapView: MKMapView!
+}
